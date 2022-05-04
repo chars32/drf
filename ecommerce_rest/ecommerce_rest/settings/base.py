739ee39386087a556fe6f41f4a6c7e5a0d4e8c9d
charsDev <chars24@gmail.com>
2022-05-03 23:54:45 -0500

    SimpleHistory, migrations, create superuser 👍

--- ecommerce_rest/ecommerce_rest/settings/base.py
+++ ecommerce_rest/ecommerce_rest/settings/base.py
@@ -27,10 +27,13 @@ BASE_APPS = [
     'django.contrib.staticfiles',
 ]
 
-LOCAL_APPS= []
+LOCAL_APPS= [
+  'apps.users',
+]
 
 THIRD_APPS = [
   'rest_framework',
+  'simple_history',
 ]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS 
@@ -43,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce_rest.urls'
@@ -95,6 +99,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
